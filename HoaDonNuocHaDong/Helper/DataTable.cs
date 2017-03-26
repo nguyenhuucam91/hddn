421810ea5d1bc219172c73399c8eaf529b3f6755
Lbee <Lbee@DESKTOP-DBLKV3M>
2017-03-26 21:19:12 +0700

    update app .dll file for server

---
+++ HoaDonNuocHaDong/Helper/DataTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoaDonNuocHaDong.Helper
+{
+    
+    public class DataTableData
+    {
+        public int TOTAL_ROWS = 995;
+        public int draw { get; set; }
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        public List<DataItem> data { get; set; }
+    }
+
+    public class DataItem
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string DoB { get; set; }
+    }
+}
