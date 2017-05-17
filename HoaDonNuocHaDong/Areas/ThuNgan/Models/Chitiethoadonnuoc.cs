af51506e7f88a13c28a79dd395b350667fdb7e35
nguyen_huu cam <nguyenhuucam91@gmail.com>
2017-05-17 15:28:23 +0700

    Resolve conflict

--- HoaDonNuocHaDong/Areas/ThuNgan/Models/Chitiethoadonnuoc.cs
+++ HoaDonNuocHaDong/Areas/ThuNgan/Models/Chitiethoadonnuoc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace HDNHD.Models.DataContexts
+namespace HoaDonNuocHaDong.Areas.ThuNgan.Models
 {
     public class Chitiethoadonnuoc
     {
