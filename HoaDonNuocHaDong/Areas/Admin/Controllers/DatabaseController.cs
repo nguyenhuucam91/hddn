18aa93b0ecde9e669374bc55cdc2370dfd586768
Lbee <Lbee@DESKTOP-DBLKV3M>
2017-05-25 14:08:39 +0700

    add gui: database restore

---
+++ HoaDonNuocHaDong/Areas/Admin/Controllers/DatabaseController.cs
@@ -0,0 +1,18 @@
+﻿using HoaDonNuocHaDong.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoaDonNuocHaDong.Areas.Admin.Controllers
+{
+    public class DatabaseController : BaseController
+    {
+        public ActionResult Restore()
+        {
+            title = "Khôi phục dữ liệu";
+            return View();
+        }
+    }
+}
