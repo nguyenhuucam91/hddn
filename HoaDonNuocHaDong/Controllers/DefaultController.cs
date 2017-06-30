c5c11ebb5588f036e5b0eb19aa4ac383880af7ed
Nguyễn Hữu Cầm <nguyenhuucam91@gmail.com>
2017-06-30 20:20:01 +0700

    Disable backup for testing

--- HoaDonNuocHaDong/Controllers/DefaultController.cs
+++ HoaDonNuocHaDong/Controllers/DefaultController.cs
@@ -15,7 +15,7 @@ namespace HoaDonNuocHaDong.Controllers
         protected BackupRepository backupRepository = new BackupRepository();
         public ActionResult Index()
         {
-            backupRepository.applyBackupProcess(LoggedInUser.NguoidungID);
+            //backupRepository.applyBackupProcess(LoggedInUser.NguoidungID);
             title = "Công ty TNHH một thành viên nước sạch Hà Đông";            
             return View();
         }
