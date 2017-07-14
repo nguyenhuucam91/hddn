a4b135d9d27e9bc8835e53dbfdb1492561b7627e
Nguyễn Hữu Cầm <nguyenhuucam91@gmail.com>
2017-07-14 16:12:09 +0700

    San Luong bat thuong va ko san luong update

--- HoaDonNuocHaDong/Models/SoLieuTieuThu/DanhSachHoaDonKhongSanLuong.cs
+++ HoaDonNuocHaDong/Models/SoLieuTieuThu/DanhSachHoaDonKhongSanLuong.cs
@@ -7,8 +7,8 @@ namespace HoaDonNuocHaDong.Models.SoLieuTieuThu
 {
     public class DanhSachHoaDonKhongSanLuong : HoaDonNuoc
     {
-        public int ChiSoCu { get { return GetINT(2); } set { SetINT(2, value); } }
+        public new int ChiSoCu { get { return GetINT(2); } set { SetINT(2, value); } }
 
-        public DateTime? NgayBatDauSuDung { get { return GetDT(3); } set { SetDT(3, value); } }
+        public new DateTime? NgayBatDauSuDung { get { return GetDT(3); } set { SetDT(3, value); } }
     }
 }
---
+++ HoaDonNuocHaDong/Models/SoLieuTieuThu/DanhSachKhachHangCoSanLuongBatThuong.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoaDonNuocHaDong.Models.SoLieuTieuThu
+{
+    public class DanhSachKhachHangCoSanLuongBatThuong : HoaDonNuoc
+    {
+        public new String SoHoaDon { get { return GetSTR(10); } set { SetSTR(10, value); } }
+        public new int SoKhoan { get { return GetINT(11); } set { SetINT(11, value); } }
+        public new int SanLuongThangTruoc { get { return GetINT(12); } set { SetINT(12, value); } }
+    }
+}
